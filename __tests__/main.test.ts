456f8be679345b8ebef5bc9cf1da2d2271ddff8b
Chris McArthur <prince.chrismc@gmail.com>
2022-01-02 19:59:08 -0800

    real test code

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,29 +1,115 @@
-import {wait} from '../src/wait'
-import * as process from 'process'
-import * as cp from 'child_process'
-import * as path from 'path'
-import {expect, test} from '@jest/globals'
+import * as core from '@actions/core'
+import * as github from '@actions/github'
+import nock from 'nock'
 
-test('throws invalid number', async () => {
-  const input = parseInt('foo', 10)
-  await expect(wait(input)).rejects.toThrow('milliseconds not a number')
-})
+import { count } from '../src/count'
+import {
+  expect,
+  test,
+  describe,
+  beforeAll,
+  beforeEach,
+  afterAll,
+  jest
+} from '@jest/globals'
 
-test('wait 500 ms', async () => {
-  const start = new Date()
-  await wait(500)
-  const end = new Date()
-  var delta = Math.abs(end.getTime() - start.getTime())
-  expect(delta).toBeGreaterThan(450)
-})
+// Inputs for mock @actions/core
+let inputs = {} as any
 
-// shows how the runner will run a javascript action with env / stdout protocol
-test('test runs', () => {
-  process.env['INPUT_MILLISECONDS'] = '500'
-  const np = process.execPath
-  const ip = path.join(__dirname, '..', 'lib', 'main.js')
-  const options: cp.ExecFileSyncOptions = {
-    env: process.env
-  }
-  console.log(cp.execFileSync(np, [ip], options).toString())
+// Shallow clone original @actions/github context
+let originalContext = { ...github.context }
+
+describe('queries', () => {
+  beforeAll(() => {
+    // Mock getInput
+    jest.spyOn(core, 'getInput').mockImplementation((name: string) => {
+      return inputs[name]
+    })
+
+    // Mock error/warning/info/debug
+    jest.spyOn(core, 'error').mockImplementation(jest.fn())
+    jest.spyOn(core, 'warning').mockImplementation(jest.fn())
+    jest.spyOn(core, 'info').mockImplementation(jest.fn())
+    jest.spyOn(core, 'debug').mockImplementation(jest.fn())
+    jest.spyOn(core, 'startGroup').mockImplementation(jest.fn())
+    jest.spyOn(core, 'endGroup').mockImplementation(jest.fn())
+    jest.spyOn(core, 'setFailed').mockImplementation(jest.fn())
+
+    // Mock github context
+    jest.spyOn(github.context, 'repo', 'get').mockImplementation(() => {
+      return {
+        owner: 'some-owner',
+        repo: 'some-repo'
+      }
+    })
+    github.context.ref = 'refs/heads/some-ref'
+    github.context.sha = '1234567890123456789012345678901234567890'
+    github.context.eventName = 'push'
+    github.context.actor = 'buster'
+  })
+
+  beforeEach(() => {
+    // Reset inputs
+    inputs = {}
+    github.context.eventName = originalContext.eventName
+    github.context.payload = originalContext.payload
+  })
+
+  afterAll(() => {
+    // Restore @actions/github context
+    github.context.ref = originalContext.ref
+    github.context.sha = originalContext.sha
+    github.context.eventName = originalContext.eventName
+    github.context.actor = originalContext.actor
+
+    // Restore
+    jest.restoreAllMocks()
+  })
+
+  test('get the count', async () => {
+    const scope = nock('https://api.github.com', {
+      reqheaders: {
+        authorization: 'token justafaketoken'
+      }
+    })
+      .get(
+        `/search/code?q=actions%2520checkout%2520language%253Ayaml%2520path%253A.github%252Fworkflows`
+      )
+      .reply(200, {
+        total_count: 101
+      })
+
+    const octokit = github.getOctokit('justafaketoken')
+
+    const counted = await count(octokit, github.context, 'actions checkout')
+    expect(counted).toBe(101)
+  })
+
+  test('encouters an error', async () => {
+    const scope = nock('https://api.github.com', {
+      reqheaders: {
+        authorization: 'token justafaketoken'
+      }
+    })
+      .get(
+        `/search/code?q=actions%2520checkout%2520language%253Ayaml%2520path%253A.github%252Fworkflows`
+      )
+      .reply(422, {
+        "message": "Validation Failed",
+        "errors": [
+          {
+            "message": "Must include at least one user, organization, or repository",
+            "resource": "Search",
+            "field": "q",
+            "code": "invalid"
+          }
+        ],
+        "documentation_url": "https://docs.github.com/v3/search/"
+      })
+
+    const octokit = github.getOctokit('justafaketoken')
+
+    const counted = count(octokit, github.context, 'actions checkout')
+    await expect(counted).rejects.toThrowError()
+  })
 })
